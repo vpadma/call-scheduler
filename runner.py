0f4825bf9d9db3fde0c77247c601e0db297ae2ba
Vinay Padma <vpadma@users.noreply.github.com>
2024-05-22 11:53:52 -0400

    Update runner.py

--- runner.py
+++ runner.py
@@ -6,6 +6,7 @@ from call_scheduler import CallScheduler
 if __name__ == '__main__':
     scheduler = CallScheduler(start_date=date(2024, 7, 1), end_date=date(2025, 6, 30))
     scheduler.create_schedule()
+    scheduler.to_csv()
     pprint.pp(
         {scheduler.schedule.residents[id].name: load for id, load in
          scheduler.schedule.total_stats.resident_load.items()})
